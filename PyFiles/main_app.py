6b8958ac7eb26e889c23cc1095c161d265cf1236
Lucian Cheng <121974540+luciancheng@users.noreply.github.com>
2023-10-05 14:13:45 -0400

    Update main_app.py

--- PyFiles/main_app.py
+++ PyFiles/main_app.py
@@ -1,6 +1,27 @@
 ''' Import Tkinter Libraries '''
 from tkinter import *
-import customer tkinter as ctk
+import customertkinter as ctk
 
 ''' Import External Classes '''
 from program_classes/user_class import cls_user
+
+''' App Class '''
+class DCM(ctk.CTk):
+  # init function to initialize the window
+  super().__init__(self):
+  self.title("G29 - MECHTRON 3K04 - DCM")
+  self.geometry("1000x700")
+  self.resizable(height=False, width=False)
+
+  # Variable definitions
+  
+  # Methods for Page navigation
+
+  # Other methods
+
+  # Functions
+
+''' Main '''
+if __name__ == "__main__":
+  dcm = DCM()
+  dcm.mainloop()
