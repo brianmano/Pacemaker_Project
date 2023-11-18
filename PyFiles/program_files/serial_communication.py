e9a23937fa1dcde12235c57df1cfac11c6d5ff3c
Brian Manomaisupat <brian.manomaisupat@gmail.com>
2023-11-18 18:47:52 -0500

    ansh stuff

--- PyFiles/program_files/serial_communication.py
+++ PyFiles/program_files/serial_communication.py
@@ -1,50 +1,30 @@
-import sys
-import glob
 import serial
 import struct
+from time import sleep
 
-ser = serial.Serial(
-    port='COM3',
-    baudrate=115200,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_TWO,
-    timeout=1,  # Set a timeout value in seconds
-    xonxoff=False,
-    rtscts=False,
-    dsrdtr=False,
-    write_timeout=None,
-    inter_byte_timeout=None
-)
+def send_packet(ser):
+        red_enable = struct.pack('B', 1)
+        green_enable = struct.pack('B', 0)
+        blue_enable = struct.pack('B', 0)
+        off_time = struct.pack('f', 3.0)
+        switch_time = struct.pack('H', 200)
+        packet = b"\x16\x55" + red_enable + green_enable + blue_enable + off_time + switch_time
+        ser.write(packet)
+        print(packet.hex())
 
-print("yes")
+def receive(s):
+    packet = b"\x16\x22" + b'\x00'*9
+    s.write(packet)
+    data = s.read(9)
+    print(data[0])
+    print(data[1])
+    print(data[2])
+    print(struct.unpack('f', data[3:7])[0])
+    print(struct.unpack('H', data[7:9])[0])
+    
+def main():
+    with serial.Serial(port='COM3',baudrate=115200,timeout=1) as s:
+        receive(s)
 
-data = ser.read(11)
-print("your mom")
-ok = data.decode('utf-8')
-#print("asdksadasdsdsadsa")
-#data_value = struct.unpack('11B', data)
-print(data)
-
-
-
-'''def serial_ports(self):
-        if sys.platform.startswith('win'):
-            ports = ['COM%s' % (i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            # this excludes your current terminal "/dev/tty"
-            ports = glob.glob('/dev/tty[A-Za-z]*')
-        elif sys.platform.startswith('darwin'):
-            ports = glob.glob('/dev/tty.*')
-        else:
-            raise EnvironmentError('Unsupported platform')
-        #print("okok")
-        result = []
-        for port in ports:
-            try:
-                s = serial.Serial(port)
-                s.close()
-                result.append(port)
-            except (OSError, serial.SerialException):
-                pass
-        return result'''
+if __name__ == "__main__":
+    main()
